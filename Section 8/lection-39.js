fe513c8461ec47868b5c9510fdfe01c0077aec21
Valentyna <vorobyova.valentina2005@gmail.com>
2026-02-10 11:23:11 +0200

    Add lection 39

---
+++ Section 8/lection-39.js
@@ -0,0 +1,10 @@
+//39.	Логічні значення (Falsy): Отримано змінну userInput.
+// Перевір, чи вона не порожня, враховуючи, що порожній рядок прирівнюється до false.
+
+let userInput = ''
+
+if(userInput) {
+    console.log(userInput)
+} else{
+    console.log('Помилка')
+}
