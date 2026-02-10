dbfd3fa4304076a6cfaa961f2ff624f67db0e16c
Valentyna <vorobyova.valentina2005@gmail.com>
2026-02-10 09:47:38 +0200

    Operator exercises Add some changes to lection 33 Add lection 34

--- Section 8/lection-33.js
+++ Section 8/lection-33.js
@@ -1,1 +1,14 @@
-console.log('Hello world')
+//33. Оператори: Здоров'я героя hp = 100.
+// Монстр завдав удару на 15 одиниць.
+// Напиши вираз, який оновлює значення hp.
+
+const hero = {
+    hp: 100
+}
+const monster = {
+    damage: 15
+}
+hero.hp -= monster.damage
+
+
+console.log(hero)
