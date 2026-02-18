36915d98c7f60bb0a4eb0d9636a2c4bde53aad68
Valentyna <vorobyova.valentina2005@gmail.com>
2026-02-18 16:57:53 +0200

    Add Section 20

---
+++ Section 20/index.js
@@ -0,0 +1,6 @@
+import { calcPlus, calcMinus } from './mathUtils.js'
+import {API_KEY} from "./lection_80-84.js";
+
+console.log(calcPlus(20, 10))
+console.log(calcMinus(40, 5))
+console.log(API_KEY)
